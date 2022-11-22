40ab0874c48450041ebe1e15389c2ddf00aef484
“yuomatsu” <07yuya08@gmail.com>
2022-11-22 18:13:46 +0900

    123_DOM要素取得（途中）

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,10 @@
-// Code goes here!
+class ProjectInput {
+  templateElement: HTMLTemplateElement;
+  hostElement: HTMLDivElement;
+
+  constructor() {
+    this.templateElement = document.getElementById('project-input')! as HTMLTemplateElement;
+    this.hostElement = document.getElementById('app')! as HTMLDivElement;
+  }
+
+}
